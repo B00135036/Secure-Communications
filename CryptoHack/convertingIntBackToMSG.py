274af6a28260288c8f6f54069cbc6d8d0d58d1cc
B00135036 <91553028+B00135036@users.noreply.github.com>
2022-10-28 22:10:34 +0100

    Delete convertingIntBackToMSG.py

--- CryptoHack/convertingIntBackToMSG.py
+++
@@ -1,9 +0,0 @@
-import codecs
-import base64
-from Crypto.Util.number import *
-
-bytesStr = codecs.decode('72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf' , 'hex_codec')
-
-base = base64.b64encode(bytesStr)
-
-print(base)
